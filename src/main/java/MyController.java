13b4d50283abe3ab55fd63b4f355a0584de96ba1
bookmarvel <anastasia.theys@gmail.com>
2024-02-10 14:08:55 -0600

    Created end game screen

--- src/main/java/MyController.java
+++ src/main/java/MyController.java
@@ -1,11 +1,14 @@
 
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -105,6 +108,25 @@ public class MyController{
     private Button bGoSleep;
 
 
+    // GAME END SCREEN  -----------------------------------------------------------------
+
+    @FXML
+    private BorderPane root2;
+
+    @FXML
+    private TextField gameEndHeader;
+
+    @FXML
+    private TextField gameEndStatus;
+
+    @FXML
+    private TextField scoreText;
+
+    @FXML
+    private TextField scoreDisplay;
+
+    // GAME MANAGEMENT FUNCTIONS  -------------------------------------------------------
+
     void updateGUI(){
         mentalHealthBar.setProgress(Main.gameState.mentalHealth / 100.0);
         foodBar.setProgress(Main.gameState.food / 100.0);
@@ -136,8 +158,47 @@ public class MyController{
 
     void checkGameLose(){
         if(Main.gameState.gameLost){
-            //TODO deal with losing the game
+            Main.gameState.gameEnded = true;
+            gameEnded();
         }
+    }
+
+    void gameEnded() {
+        int score = Main.gameState.calculateScore();
+
+        String status = "Game ended because ";
+
+        if (Main.gameState.mentalHealth <= 0) {
+            status = status + "you didn't take good enough care of your mental health";
+        } else if (Main.gameState.food <= 0) {
+            status = status + "you didn't eat enough food";
+        } else {
+            status = status + "you survived a week!";
+        }
+
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/gameEndScreen.fxml"));
+            Parent newRoot = loader.load();
+
+            root.getScene().setRoot(newRoot);
+            newRoot.getScene().getStylesheets().add("/styles/mainStyle.css");
+
+            MyController newController = loader.getController();
+
+            newController.gameEndStatus.setText(status);
+
+            newController.scoreDisplay.setText(String.valueOf(score));
+
+
+        } catch (IOException e){
+            System.out.println("Error: unable to load fxml file");
+            e.printStackTrace();
+
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+
     }
 
     // EVENT HANDLERS -------------------------------------------------------------------
@@ -216,7 +277,7 @@ public class MyController{
         bMakeSnack.setDisable(false);
 
         if(Main.gameState.gameEnded){
-            //TODO things when game ends
+           gameEnded();
         }
     }
 }
