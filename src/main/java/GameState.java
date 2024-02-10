46ad2693586a3857ebba0e7e0ef0ed6e0b9d64ff
Chris Wood <118241499+Cwood00@users.noreply.github.com>
2024-02-10 10:42:19 -0600

    Implemented Goto sleep function

--- src/main/java/GameState.java
+++ src/main/java/GameState.java
@@ -1,24 +1,27 @@
+import java.util.concurrent.BrokenBarrierException;
+
 public class GameState {
-    public enum WeekDay {
+    public enum DaysOfTheWeek {
         SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
     }
-    WeekDay day;
+    DaysOfTheWeek day;
     int time;//Minutes since start of day
-    int mentalHealth; //Between 0-100 inclusive
-    int food; //Between 0-100 inclusive
-    int money;
+    int mentalHealth = 50; //Between 0-100 inclusive
+    int food = 50; //Between 0-100 inclusive
+    int money = 100;
     int hoursWorked;
     double hoursHomeworkDone;
     double hoursOfChoresDone;
     double hoursOfClassDone;
     boolean gameLost;
+    boolean gameEnded;
 
     private void addFood(int newFood){
         this.food += newFood;
         if(this.food > 100) {
             this.food = 100;
         }
-        else if(this.food < 0){
+        else if(this.food <= 0){
             gameLost = true;
         }
     }
@@ -27,7 +30,14 @@ public class GameState {
         if(this.mentalHealth > 100){
             this.mentalHealth = 100;
         }
-        else if(this.mentalHealth < 0){
+        else if(this.mentalHealth <= 0){
+            gameLost = true;
+        }
+    }
+
+    private void addMoney(int newMoney){
+        this.money += newMoney;
+        if(this.money < 0){
             gameLost = true;
         }
     }
@@ -40,11 +50,41 @@ public class GameState {
     }
 
     public void gotoSleep(){
-
+        int minutesSlept = 1440 - time;
+        if(minutesSlept >= 480){
+            addMentalHealth(20);
+        }
+        else{
+            addMentalHealth(20 - ((480 - minutesSlept) / 3));
+        }
+        switch (day){
+            case SUNDAY:
+                day = DaysOfTheWeek.SUNDAY;
+            case MONDAY:
+                day = DaysOfTheWeek.TUESDAY;
+                break;
+            case TUESDAY:
+                day = DaysOfTheWeek.WEDNESDAY;
+                break;
+            case WEDNESDAY:
+                day = DaysOfTheWeek.THURSDAY;
+                break;
+            case THURSDAY:
+                day = DaysOfTheWeek.FRIDAY;
+                break;
+            case FRIDAY:
+                day = DaysOfTheWeek.SATURDAY;
+                break;
+            case SATURDAY:
+                day = DaysOfTheWeek.SUNDAY;
+                gameEnded = true;
+                break;
+        }
     }
     public void doHomework(){
 
     }
+
     public void doWork(){
 
     }
